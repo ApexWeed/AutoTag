85f57bd760d9867edd6930f9e533e5c294011520
ApexWeed <heli.freak.95@gmail.com>
2016-08-18 07:37:42 +1000

    Initial commit.

---
+++ Auto Tag/Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace Auto_Tag
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstDisplay = new System.Windows.Forms.ListView();
+            this.Input = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Output = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.btnProcess = new System.Windows.Forms.Button();
+            this.btnSaveTags = new System.Windows.Forms.Button();
+            this.txtInputFormat = new System.Windows.Forms.TextBox();
+            this.txtOutputFormat = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnRename = new System.Windows.Forms.Button();
+            this.pgbProgress = new System.Windows.Forms.ProgressBar();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lstDisplay
+            // 
+            this.lstDisplay.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.Input,
+            this.Output});
+            this.lstDisplay.Location = new System.Drawing.Point(12, 79);
+            this.lstDisplay.Name = "lstDisplay";
+            this.lstDisplay.Size = new System.Drawing.Size(839, 409);
+            this.lstDisplay.TabIndex = 0;
+            this.lstDisplay.UseCompatibleStateImageBehavior = false;
+            this.lstDisplay.View = System.Windows.Forms.View.Details;
+            // 
+            // Input
+            // 
+            this.Input.Text = "Input";
+            this.Input.Width = 471;
+            // 
+            // Output
+            // 
+            this.Output.Text = "Output";
+            this.Output.Width = 364;
+            // 
+            // btnProcess
+            // 
+            this.btnProcess.Location = new System.Drawing.Point(695, 10);
+            this.btnProcess.Name = "btnProcess";
+            this.btnProcess.Size = new System.Drawing.Size(75, 21);
+            this.btnProcess.TabIndex = 1;
+            this.btnProcess.Text = "Process";
+            this.btnProcess.UseVisualStyleBackColor = true;
+            this.btnProcess.Click += new System.EventHandler(this.btnProcess_Click);
+            // 
+            // btnSaveTags
+            // 
+            this.btnSaveTags.Location = new System.Drawing.Point(695, 41);
+            this.btnSaveTags.Name = "btnSaveTags";
+            this.btnSaveTags.Size = new System.Drawing.Size(75, 21);
+            this.btnSaveTags.TabIndex = 2;
+            this.btnSaveTags.Text = "Save Tags";
+            this.btnSaveTags.UseVisualStyleBackColor = true;
+            this.btnSaveTags.Click += new System.EventHandler(this.btnSaveTags_Click);
+            // 
+            // txtInputFormat
+            // 
+            this.txtInputFormat.Location = new System.Drawing.Point(92, 13);
+            this.txtInputFormat.Name = "txtInputFormat";
+            this.txtInputFormat.Size = new System.Drawing.Size(588, 19);
+            this.txtInputFormat.TabIndex = 3;
+            this.txtInputFormat.Text = "%a - %t";
+            // 
+            // txtOutputFormat
+            // 
+            this.txtOutputFormat.Location = new System.Drawing.Point(92, 42);
+            this.txtOutputFormat.Name = "txtOutputFormat";
+            this.txtOutputFormat.Size = new System.Drawing.Size(588, 19);
+            this.txtOutputFormat.TabIndex = 4;
+            this.txtOutputFormat.Text = "%a - %t";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 12);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Input Format";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 12);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Output Format";
+            // 
+            // btnRename
+            // 
+            this.btnRename.Location = new System.Drawing.Point(776, 10);
+            this.btnRename.Name = "btnRename";
+            this.btnRename.Size = new System.Drawing.Size(75, 21);
+            this.btnRename.TabIndex = 8;
+            this.btnRename.Text = "Rename";
+            this.btnRename.UseVisualStyleBackColor = true;
+            this.btnRename.Click += new System.EventHandler(this.btnRename_Click);
+            // 
+            // pgbProgress
+            // 
+            this.pgbProgress.Location = new System.Drawing.Point(12, 494);
+            this.pgbProgress.Name = "pgbProgress";
+            this.pgbProgress.Size = new System.Drawing.Size(839, 21);
+            this.pgbProgress.TabIndex = 9;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(776, 41);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 21);
+            this.btnClear.TabIndex = 10;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 64);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(339, 12);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "%t - Title, %a - Artist, %A - Album, %y - Year, %T - Track Number";
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(863, 527);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.pgbProgress);
+            this.Controls.Add(this.btnRename);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtOutputFormat);
+            this.Controls.Add(this.txtInputFormat);
+            this.Controls.Add(this.btnSaveTags);
+            this.Controls.Add(this.btnProcess);
+            this.Controls.Add(this.lstDisplay);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.Form1_DragEnter);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView lstDisplay;
+        private System.Windows.Forms.ColumnHeader Input;
+        private System.Windows.Forms.ColumnHeader Output;
+        private System.Windows.Forms.Button btnProcess;
+        private System.Windows.Forms.Button btnSaveTags;
+        private System.Windows.Forms.TextBox txtInputFormat;
+        private System.Windows.Forms.TextBox txtOutputFormat;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnRename;
+        private System.Windows.Forms.ProgressBar pgbProgress;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Auto Tag/Form1.cs
@@ -0,0 +1,323 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using TagLib;
+
+namespace Auto_Tag
+{
+    public partial class Form1 : Form
+    {
+        BackgroundWorker SaveTagsWorker;
+        BackgroundWorker ProcessWorker;
+
+        List<string> InputList = new List<string>();
+        List<string> OutputList = new List<string>();
+        List<Dictionary<string, string>> OutputOperations = new List<Dictionary<string, string>>();
+        public Form1()
+        {
+            InitializeComponent();
+            
+            SaveTagsWorker = new BackgroundWorker();
+            SaveTagsWorker.DoWork += SaveTagsWorker_DoWork;
+            SaveTagsWorker.ProgressChanged += SaveTagsWorker_ProgressChanged;
+            SaveTagsWorker.RunWorkerCompleted += SaveTagsWorker_RunWorkerCompleted;
+            SaveTagsWorker.WorkerReportsProgress = true;
+
+            ProcessWorker = new BackgroundWorker();
+            ProcessWorker.DoWork += ProcessWorker_DoWork;
+            ProcessWorker.ProgressChanged += ProcessWorker_ProgressChanged;
+            ProcessWorker.RunWorkerCompleted += ProcessWorker_RunWorkerCompleted;
+            ProcessWorker.WorkerReportsProgress = true;
+        }
+
+        private void Form1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effect = DragDropEffects.Copy;
+            else
+                e.Effect = DragDropEffects.None;
+        }
+
+        private void Form1_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            foreach (string file in files)
+            {
+                InputList.Add(file);
+                OutputList.Add("");
+            }
+            UpdateDisplay();
+        }
+
+        private void UpdateDisplay()
+        {
+            lstDisplay.Items.Clear();
+            string[] item = new string[2];
+            for (int i = 0; i < InputList.Count; i++)
+            {
+                item[0] = Path.GetFileNameWithoutExtension(InputList[i]);
+                item[1] = Path.GetFileNameWithoutExtension(OutputList[i]);
+                lstDisplay.Items.Add(new ListViewItem(item));
+            }
+        }
+
+        /*
+         * Tags:
+         * %t - Title
+         * %a - Artist
+         * %A - Album
+         * %y - Year
+         * %T - Track Number
+         */
+
+        private void btnProcess_Click(object sender, EventArgs e)
+        {
+            btnProcess.Enabled = false;
+            btnSaveTags.Enabled = false;
+            ProcessWorker.RunWorkerAsync();
+        }
+
+        private void ProcessWorker_DoWork(object sender, System.ComponentModel.DoWorkEventArgs e)
+        {
+            OutputList.Clear();
+
+            string path;
+            string filename;
+            string extension;
+
+            string title;
+            string artist;
+            string album;
+            string year;
+            string track;
+
+            string output;
+            string inputFormat;
+
+            Dictionary<int, string> tokens = new Dictionary<int, string>();
+
+            string[] operationsArray;
+
+            // Find all input tags.
+
+            if (txtInputFormat.Text.IndexOf("%t") != -1) // Title.
+            {
+                tokens.Add(txtInputFormat.Text.IndexOf("%t"), "%t");
+            }
+            if (txtInputFormat.Text.IndexOf("%a") != -1) // Artist.
+            {
+                tokens.Add(txtInputFormat.Text.IndexOf("%a"), "%a");
+            }
+            if (txtInputFormat.Text.IndexOf("%A") != -1) // Album.
+            {
+                tokens.Add(txtInputFormat.Text.IndexOf("%A"), "%A");
+            }
+            if (txtInputFormat.Text.IndexOf("%y") != -1) // Year.
+            {
+                tokens.Add(txtInputFormat.Text.IndexOf("%y"), "%y");
+            }
+            if (txtInputFormat.Text.IndexOf("%T") != -1) // Track.
+            {
+                tokens.Add(txtInputFormat.Text.IndexOf("%T"), "%T");
+            }
+
+            List<int> keys = tokens.Keys.ToList<int>();
+
+            // Sort ascending.
+            keys.Sort();
+            int currentIndex = 0;
+
+            // Replace tags.
+            inputFormat = txtInputFormat.Text;
+            foreach (int key in keys)
+            {
+                inputFormat = inputFormat.Replace(tokens[key], string.Format("{{{0}}}", currentIndex));
+                currentIndex++;
+            }
+
+            // Convert output string into string.format format.
+            output = txtOutputFormat.Text;
+            output = output.Replace("%t", "{0}"); // Title.
+            output = output.Replace("%a", "{1}"); // Artist.
+            output = output.Replace("%A", "{2}"); // Album.
+            output = output.Replace("%y", "{3}"); // Year.
+            output = output.Replace("%T", "{4}"); // Track.
+
+            OutputOperations.Clear();
+
+            for (int i = 0; i < InputList.Count; i++)
+            {
+                Dictionary<string, string> operations = new Dictionary<string, string>();
+                operationsArray = Path.GetFileNameWithoutExtension(InputList[i]).ParseExact(inputFormat);
+
+                //title = "";
+                //artist = "";
+                //album = "";
+                //year = "";
+
+                title = "Title";
+                artist = "Artist";
+                album = "Album";
+                year = "Year";
+                track = "Track";
+
+                // Process each operation.
+                for (int j = 0; j < operationsArray.Length; j++)
+                {
+                    switch (tokens[keys[j]])
+                    {
+                        case "%t":
+                            title = operationsArray[j];
+                            break;
+                        case "%a":
+                            artist = operationsArray[j];
+                            break;
+                        case "%A":
+                            album = operationsArray[j];
+                            break;
+                        case "%y":
+                            year = operationsArray[j];
+                            break;
+                        case "%T":
+                            track = operationsArray[j];
+                            break;
+                    }
+                }
+
+                // Split filename.
+                path = Path.GetDirectoryName(InputList[i]);
+                filename = Path.GetFileNameWithoutExtension(InputList[i]);
+                extension = Path.GetExtension(InputList[i]);
+
+                OutputList.Add(string.Format("{0}\\{1}{2}", path, string.Format(output, title, artist, album, year, track), extension));
+
+                // Add operations for LibTag if they are set.
+                if (title != "Title")
+                    operations.Add("Title", title);
+                if (artist != "Artist")
+                    operations.Add("Artist", artist);
+                if (album != "Album")
+                    operations.Add("Album", album);
+                if (year != "Year")
+                    operations.Add("Year", year);
+                if (track != "Track")
+                    operations.Add("Track", track);
+
+                OutputOperations.Add(operations);
+
+                ProcessWorker.ReportProgress(i);
+            }
+        }
+
+        private void ProcessWorker_ProgressChanged(object sender, System.ComponentModel.ProgressChangedEventArgs e)
+        {
+            pgbProgress.Value = (int)(((float)e.ProgressPercentage / (float)InputList.Count) * 100);
+        }
+
+        private void ProcessWorker_RunWorkerCompleted(object sender, System.ComponentModel.RunWorkerCompletedEventArgs e)
+        {
+            if (e.Error != null)
+            {
+                MessageBox.Show(e.Error.Message);
+            }
+            else if (e.Cancelled)
+            {
+                MessageBox.Show("Cancelled");
+            }
+
+            btnProcess.Enabled = true;
+            btnSaveTags.Enabled = true;
+            pgbProgress.Value = 0;
+            UpdateDisplay();
+        }
+
+        private void btnSaveTags_Click(object sender, EventArgs e)
+        {
+            btnSaveTags.Enabled = false;
+            btnProcess.Enabled = false;
+            SaveTagsWorker.RunWorkerAsync();
+        }
+
+        private void SaveTagsWorker_DoWork(object sender, System.ComponentModel.DoWorkEventArgs e)
+        {
+            for (int i = 0; i < InputList.Count; i++)
+            {
+                TagLib.File tagFile = TagLib.File.Create(InputList[i]);
+                List<string> keys = OutputOperations[i].Keys.ToList<string>();
+                for (int j = 0; j < OutputOperations[i].Count; j++)
+                {
+                    switch (keys[j])
+                    {
+                        case "Title":
+                            tagFile.Tag.Title = OutputOperations[i][keys[j]];
+                            break;
+                        case "Artist":
+                            tagFile.Tag.AlbumArtists = null;
+                            tagFile.Tag.Performers = null;
+                            List<string> artists = new List<string>(OutputOperations[i][keys[j]].Split(','));
+                            artists.ForEach(a => a = a.Trim());
+                            tagFile.Tag.AlbumArtists = artists.ToArray();
+                            tagFile.Tag.Performers = artists.ToArray();
+                            break;
+                        case "Album":
+                            tagFile.Tag.Album = OutputOperations[i][keys[j]];
+                            break;
+                        case "Year":
+                            tagFile.Tag.Year = uint.Parse(OutputOperations[i][keys[j]]);
+                            break;
+                        case "Track":
+                            tagFile.Tag.Track = uint.Parse(OutputOperations[i][keys[j]]);
+                            break;
+                    }
+                }
+                tagFile.Save();
+                SaveTagsWorker.ReportProgress(i);
+            }
+        }
+
+        private void SaveTagsWorker_ProgressChanged(object sender, System.ComponentModel.ProgressChangedEventArgs e)
+        {
+            pgbProgress.Value = (int)(((float)e.ProgressPercentage / (float)InputList.Count) * 100);
+        }
+
+        private void SaveTagsWorker_RunWorkerCompleted(object sender, System.ComponentModel.RunWorkerCompletedEventArgs e)
+        {
+            if (e.Error != null)
+            {
+                MessageBox.Show(e.Error.Message);
+            }
+            else if (e.Cancelled)
+            {
+                MessageBox.Show("Cancelled");
+            }
+
+            btnSaveTags.Enabled = true;
+            btnProcess.Enabled = true;
+            pgbProgress.Value = 0;
+        }
+
+        private void btnRename_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private int CountInString(string haystack, string needle)
+        {
+            return ((haystack.Length - haystack.Replace(needle, "").Length) / needle.Length);
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            lstDisplay.Items.Clear();
+            InputList.Clear();
+            OutputList.Clear();
+        }
+    }
+}
